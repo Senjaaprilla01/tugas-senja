fcdeebb807de3f08a45e2f944110ada626317d94
senja aprilla handrina setia <senjaaprilla14@gmail.com>
2025-07-09 16:50:19 +0700

    Add files via upload

---
+++ Pertemuan-7/soal1.js
@@ -0,0 +1,9 @@
+// Problem 1 - String Concatenation
+let firstName = 'Senja';
+let lastName = 'Aprilla';
+let bornYear = 2005;
+
+let currentYear = 2005;
+let age = currentYear - bornYear;
+
+console.log('Hello, ' + firstName + ' ' + lastName + '! You are ' + age + ' years old.');
